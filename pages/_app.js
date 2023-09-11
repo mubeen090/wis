eb30ca2248eadf9ed3a6a8933b19d5628d488c4f
mubeen090 <144161677+mubeen090@users.noreply.github.com>
2023-09-12 04:46:45 +0530

    first commit

--- pages/_app.js
+++
@@ -1,9 +0,0 @@
-import "../styles/tailwind.css";
-import "../styles/slick.css";
-import "../styles/globalStyles.css";
-
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
-}
-
-export default MyApp;
